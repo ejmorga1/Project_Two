4b15df687293c310ccf4ee803ac29d6f7fcc4011
“dylanburkey” <dylanburkey@outlook.com>
2018-08-16 18:54:50 -0400

    Updated Files

---
+++ gulpfile.js
@@ -0,0 +1,54 @@
+
+  var gulp      = require('gulp');
+  var sass      = require('gulp-sass');
+  var minifyCss = require('gulp-minify-css');
+  var uglify    = require('gulp-uglify');
+  var concat    = require('gulp-concat');
+  //var imagemin  = require('gulp-imagemin');
+  //var pngquant  = require('imagemin-pngquant');
+
+  var paths = {
+    sass    : './public-src/scss/**/*.scss',
+    css     : './public/css',
+    js_src  : './public-src/js/**/*.js',
+    js      : './public/js',
+    img_src : './public-src/images/**/*',
+    images  : './public/images'
+  };
+
+  gulp.task('css', function(){
+    return gulp.src(paths.sass)
+      .pipe(sass())
+      .pipe(minifyCss())
+      .pipe(gulp.dest(paths.css));
+  });
+
+  gulp.task('js', function(){
+    return gulp.src(paths.js_src)
+      .pipe(concat('dist.js'))
+      .pipe(uglify())
+      .pipe(gulp.dest(paths.js));
+  });
+
+  /*
+    "gulp-imagemin"     : "^2",
+    "imagemin-pngquant" : "^4"
+  gulp.task('images', function () {
+    return gulp.src(paths.img_src)
+        .pipe(imagemin({
+            progressive : true,
+            svgoPlugins : [{removeViewBox: false}],
+            use         : [pngquant()]
+        }))
+        .pipe(gulp.dest(paths.images));
+  });
+  */
+
+  gulp.task('watch', function(){
+    gulp.watch(paths.sass, ['css']);
+    gulp.watch(paths.js_src, ['js']);
+    //gulp.watch(paths.img_src, ['images']);
+  });
+
+
+  gulp.task('default', ['css', 'js', 'watch']); //  'images',
