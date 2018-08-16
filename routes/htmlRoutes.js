3b0b102223f510506a7d64510d457cb38cb96951
Freeman <ejmorga1@gmail.com>
2018-08-16 18:53:37 -0400

    new models

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -1,7 +1,7 @@
 var db = require("../models");
 
 module.exports = function(app) {
-  // Load index page
+
   app.get("/", function(req, res) {
     db.Example.findAll({}).then(function(dbExample) {
       res.render("index", {
@@ -10,7 +10,6 @@ module.exports = function(app) {
     });
   });
 
-  // Load example page and pass in an example by id
   app.get("/:id/:username", function(req, res) {
     db.Example.findOne({
       where: {
@@ -23,7 +22,6 @@ module.exports = function(app) {
     });
   });
 
-  // Render 404 page for any unmatched routes
   app.get("*", function(req, res) {
     res.render("404");
   });
