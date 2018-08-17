2182f4585ff2e8984ca23b84867aacf73e236227
Freeman <ejmorga1@gmail.com>
2018-08-16 20:21:35 -0400

    new branch

--- server.js
+++ server.js
@@ -22,15 +22,19 @@ app.engine(
 );
 app.set("view engine", "handlebars");
 
-require("./routes/apiRoutesuserProfiles")(app);
-require("./routes/apiRoutesusers")(app);
+// Routes
+require("./routes/apiRoutes")(app);
 require("./routes/htmlRoutes")(app);
 
 var syncOptions = { force: false };
+
+// If running a test, set syncOptions.force to true
+// clearing the `testdb`
 if (process.env.NODE_ENV === "test") {
   syncOptions.force = true;
 }
 
+// Starting the server, syncing our models ------------------------------------/
 db.sequelize.sync(syncOptions).then(function() {
   app.listen(PORT, function() {
     console.log(
